18b052fcaacd550231e4006b99c983d19a344faa
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2019-01-19 17:01:38 +0400

    Show good job dialog when game is over

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,23 @@
 import '../scss/style.scss';
 import PicturePuzzle from './PicturePuzzle'
 
-const puzzle = new PicturePuzzle(document.body,
-  'https://www.tesla.com/ns_videos/homepage/homepage-models.jpg?02018018018',
-  3, 600);
+window.addEventListener('load', init);
+
+function init() {
+  const puzzle = new PicturePuzzle(document.querySelector('#puzzle-wrapper'),
+    'https://www.tesla.com/ns_videos/homepage/homepage-models.jpg?02018018018',
+    3, 600);
+
+  const modal = document.querySelector('#success-modal');
+  modal.style.display = 'block';
+  puzzle.onDone = function () {
+    setTimeout(() => {
+      modal.classList.add('open');
+      this.el.classList.add('blur-it');
+      modal.querySelector('.trigger').onclick = () => {
+        modal.classList.remove('open');
+        this.el.classList.remove('blur-it');
+      }
+    }, 500);
+  };
+}
